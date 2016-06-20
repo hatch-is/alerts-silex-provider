036e1babbf226bcad60fc27a4a292c98fde3d4ad
ditrovskiy <vladimir@tep.io>
2016-06-20 16:46:22 +0300

    Feat: add processor basics

---
+++ src/AlertsSilexProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Alerts;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * Class AlertsSilexProvider
+ *
+ * @package Alerts
+ */
+class AlertsSilexProvider implements ServiceProviderInterface
+{
+    /**
+     * Registers services on the given app.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     */
+    public function register(Application $app)
+    {
+        $app['hatch-is.alerts.processor'] = $app->share(
+            function () use ($app) {
+                return new Processor(
+                    $app['hatch-is.alerts.endpoint']
+                );
+            }
+        );
+    }
+
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     */
+    public function boot(Application $app)
+    {
+
+    }
+}
---
+++ src/Processor.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Alerts;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException as GuzzleClientException;
+use GuzzleHttp\Psr7\Request;
+use Symfony\Component\Security\Acl\Exception\Exception;
+
+/**
+ * Class Processor
+ *
+ * @package Alerts
+ */
+class Processor
+{
+    protected $endpoint;
+
+    public function __construct($endpoint)
+    {
+        if (null === $endpoint) {
+            throw new Exception(
+                'Alerts service: endpoint is null'
+            );
+        }
+
+        $this->endpoint = $endpoint;
+    }
+
+    protected function getPath($path)
+    {
+        return $this->endpoint . $path;
+    }
+
+    /**
+     * @param GuzzleClient $client
+     * @param Request      $request
+     *
+     * @return \Psr\Http\Message\StreamInterface
+     * @throws \Exception
+     */
+    public function send(GuzzleClient $client, Request $request)
+    {
+        try {
+            $response = $client->send($request);
+            return $response->getBody();
+        } catch (GuzzleClientException $e) {
+            $message = $this->formatErrorMessage($e);
+            throw new \Exception(json_encode($message), 0, $e);
+        }
+    }
+
+    /**
+     * @param GuzzleClientException $httpException
+     *
+     * @return array
+     */
+    protected function formatErrorMessage($httpException)
+    {
+        $message = [
+            'message'  => 'Something bad happened with statistic service',
+            'request'  => [
+                'headers' => $httpException->getRequest()->getHeaders(),
+                'body'    => $httpException->getRequest()->getBody()
+            ],
+            'response' => [
+                'headers' => $httpException->getResponse()->getHeaders(),
+                'body'    => $httpException->getResponse()->getBody()
+                    ->getContents(),
+                'status'  => $httpException->getResponse()->getStatusCode()
+            ]
+        ];
+
+        return $message;
+    }
+}
